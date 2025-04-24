903b28683e8d55c62f60532769ca9e8273cb9372
Sin11021219 <sinya11021219@gmail.com>
2025-04-25 01:51:00 +0900

    iam2

---
+++ iam2.tf
@@ -0,0 +1,32 @@
+#-----------------------------------------
+#IAM Role
+#-----------------------------------------
+#SSM  Managed Instance Core
+data "aws_iam_policy" "ssm_managed_instancecore" {
+  #許可ポリシー
+  name = "AmazonSSMManagedInstanceCore"
+  ｝
+
+resource "aws_iam_role" "ssm_role" {
+  # 信頼ポリシー
+  name = "${var.poject}-ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+#IAMロールにAssumeSSMManagedInstanceCoreポリシーをアタッチ
+resource "aws_iam_role_policy_attachment" "allow_ssm_policy" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = data.aws_iam_policy.ssm_managed_instancecore.arn 
+}
