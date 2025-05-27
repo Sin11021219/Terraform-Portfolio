8d82d1a00b7aab3803d762c64232d9d4698cae0b
Sin11021219 <sinya11021219@gmail.com>
2025-05-28 02:13:06 +0900

    update

--- alb.tf
+++ alb.tf
@@ -73,4 +73,3 @@ resource "aws_lb_target_group_attachment" "target_instance_2" {
   target_group_arn = aws_lb_target_group.aws_lb_target_group.arn
   target_id        = aws_instance.cloudtech_webserver_ec2_2.id
 }
-
